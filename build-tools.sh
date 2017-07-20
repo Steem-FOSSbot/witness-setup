802b079ad6bc833336752b4346f07a3b0ede058d
thrize <thrize@tutanota.de>
2017-07-20 05:55:15 -0400

    Update build script to v0.19.1rc1

--- build-tools.sh
+++ build-tools.sh
@@ -25,21 +25,31 @@ fi
 # update system
 echo $CYAN"Installing dependencies..."$RESET
 apt-get update
-apt-get install -y autoconf automake cmake g++ git libssl-dev libtool make pkg-config
+apt-get install -y autoconf automake g++ git libssl-dev libtool make pkg-config python3 python3-jinja2
 apt-get install -y libboost-chrono-dev libboost-context-dev libboost-coroutine-dev libboost-date-time-dev libboost-filesystem-dev libboost-iostreams-dev libboost-locale-dev libboost-program-options-dev libboost-serialization-dev libboost-signals-dev libboost-system-dev libboost-test-dev libboost-thread-dev
 apt-get install -y doxygen libncurses5-dev libreadline-dev perl
+
+pip install Jinja2
+
+echo pushd /tmp >/dev/null
+echo wget http://www.cmake.org/files/v3.9/cmake-3.9.0.tar.gz
+echo tar xf cmake-3.9.0.tar.gz
+echo cd cmake-3.9.0
+echo ./configure
+echo make
+echo popd >/dev/null
 
 echo $CYAN"Downloading steem source..."$RESET
 pushd ~ >/dev/null
 git clone https://github.com/steemit/steem
 cd steem
-git checkout stable
+git checkout v0.19.1rc1
 
 echo $CYAN"Set up build environment..."$RESET
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake -DLOW_MEMORY_NODE=ON -DCMAKE_BUILD_TYPE=Release ..
+cmake -DLOW_MEMORY_NODE=ON -DCMAKE_BUILD_TYPE=Release -DBOOST_ROOT=/usr/local ..
 
 echo $CYAN"Make tools..."$RESET
 echo $CYAN"(1/2) Make steemd..."$RESET
